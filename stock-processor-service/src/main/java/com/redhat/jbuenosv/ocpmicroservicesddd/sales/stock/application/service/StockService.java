312af6b07f0b9b16891273e2a4f6e30321079140
Juan Luis Buenosvinos <jlbuenosvinos@gmail.com>
2018-09-10 11:20:18 +0200

    v0.1.1

--- stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/service/StockService.java
+++ stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/service/StockService.java
@@ -1,9 +1,5 @@
 package com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.application.service;
-
-import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.StockValue;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.Ticket;
-
-import java.util.List;
 
 /**
  * This service includes the main logic for the ticketing projection service
@@ -16,20 +12,5 @@ public interface StockService {
      * @param ticket ticket to be processed
      */
     void processTicket(Ticket ticket);
-
-    /**
-     * Get the current stock for an specific product and store
-     * @param storeId store id
-     * @return stock value
-     */
-    List<StockValue> getStock(Integer storeId);
-
-    /**
-     * Get the current stock for an specific product and store
-     * @param storeId store id
-     * @param productId product id
-     * @return stock value
-     */
-    StockValue getStock(Integer storeId, String productId);
 
 }
--- stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/service/StockServiceImpl.java
+++ stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/service/StockServiceImpl.java
@@ -6,15 +6,12 @@ import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.StockKey
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.StockValue;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.Ticket;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.infrastructure.domain.event.DomainEventPublisher;
-import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.infrastructure.repository.StockRepository;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.infrastructure.store.StockEventStore;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.infrastructure.util.UUIDGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * This service includes the main logic for the ticketing projection service
@@ -33,9 +30,6 @@ public class StockServiceImpl implements StockService {
 
     @Autowired
     CommonConfig config;
-
-    @Autowired
-    private StockRepository stockRepository;
 
     @Autowired
     private StockEventStore stockEventStore;
@@ -62,26 +56,6 @@ public class StockServiceImpl implements StockService {
 
         eventPublisher.publish(stockEvent);
         logger.debug("Ticket [{}] has been processed.",ticket.getTicketId());
-
-    }
-
-    /**
-     * Get the current stock for an specific product and store
-     * @param storeId store id
-     * @return stock value
-     */
-    public List<StockValue> getStock(Integer storeId) {
-        return stockRepository.findByByStoreId(storeId);
-    }
-
-    /**
-     * Get the current stock for an specific product and store
-     * @param storeId store id
-     * @param productId product id
-     * @return stock value
-     */
-    public StockValue getStock(Integer storeId, String productId) {
-        return stockRepository.findByStoreIdProductId(storeId,productId);
     }
 
 }
