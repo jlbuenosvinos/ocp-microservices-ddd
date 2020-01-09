d0fcef957dd68850b5223c0e499cd3a5c4164675
Juan Luis Buenosvinos <jlbuenosvinos@gmail.com>
2020-01-09 14:29:37 +0100

    no message

--- stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/cache/StockCacheFactory.java
+++ stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/cache/StockCacheFactory.java
@@ -64,7 +64,7 @@ public class StockCacheFactory implements CacheFactory {
 
             builder.transaction()
                     .transactionManagerLookup(GenericTransactionManagerLookup.getInstance())
-                    .transactionMode(TransactionMode.NON_XA);
+                    .transactionMode(TransactionMode.NONE);
 
             logger.debug("Configuration security.");
 
