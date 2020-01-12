2541d353a5c72378e57ed2a43ecca2c49b263683
Juan Luis Buenosvinos <jlbuenosvinos@gmail.com>
2020-01-12 11:51:13 +0100

    no message

--- stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/cache/StockRemoteCacheFactory.java
+++ stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/cache/StockRemoteCacheFactory.java
@@ -83,9 +83,9 @@ public class StockRemoteCacheFactory implements CacheFactory {
                     "<infinispan>" +
                             "<cache-container>" +
                                 "<distributed-cache name=\"%1$s\">" +
-                                    "<transaction mode=\"NON_XA\" />" +
+                                    "<transaction mode=\"NON_XA\" protocol=\"DEFAULT\"/>" +
                                     //"<transaction mode=\"NON_XA\" locking=\"PESSIMISTIC\" />" +
-                                    // "<locking isolation=\"REPEATABLE_READ\" acquire-timeout=\"30000\" concurrency-level=\"1000\" striping=\"false\"/>" +
+                                    "<locking isolation=\"REPEATABLE_READ\" acquire-timeout=\"30000\" concurrency-level=\"1000\" striping=\"false\"/>" +
                                 "</distributed-cache>" +
                             "</cache-container>" +
                     "</infinispan>",
