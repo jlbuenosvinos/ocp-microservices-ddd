4f57a3107dc221b2e61f578b9ca72dfc7e5a36d6
Juan Luis Buenosvinos <jlbuenosvinos@gmail.com>
2020-01-21 13:15:51 +0100

    no message

--- stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/controller/kafka/TicketingKafkaController.java
+++ stock-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/infrastructure/controller/kafka/TicketingKafkaController.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jlbuenosvinos.
