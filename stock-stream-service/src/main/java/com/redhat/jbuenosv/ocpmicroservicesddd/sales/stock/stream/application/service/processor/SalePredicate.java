09a5ae8735b93c3bf1316c28a0467a253c390c99
Juan Luis Buenosvinos <jlbuenosvinos@gmail.com>
2020-01-30 11:15:59 +0100

    no message

--- stock-stream-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/stream/application/service/processor/SalePredicate.java
+++ stock-stream-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/stream/application/service/processor/SalePredicate.java
@@ -3,15 +3,27 @@ package com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.stream.application.s
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.stream.domain.model.TicketKey;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.stream.domain.model.TicketTotalValue;
 import org.apache.kafka.streams.kstream.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by jlbuenosvinos.
  */
 public class SalePredicate implements Predicate<TicketKey, TicketTotalValue> {
 
+    public static final Logger logger = LoggerFactory.getLogger(SalePredicate.class);
+
     @Override
     public boolean test(TicketKey ticketKey, TicketTotalValue ticketTotalValue) {
-        return ticketTotalValue.getUnits() > 0;
+
+        if (ticketTotalValue != null) {
+            return ticketTotalValue.getUnits() > 0;
+        }
+        else {
+            logger.warn("TicketTotalValue is NULL !!!.");
+            return false;
+        }
+
     }
 
 }
