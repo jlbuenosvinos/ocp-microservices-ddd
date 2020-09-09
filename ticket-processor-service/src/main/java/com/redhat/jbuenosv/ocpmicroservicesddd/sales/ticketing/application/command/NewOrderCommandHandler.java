b2f5abd776a0780463fad6a28f514de9ad4151a9
Juan Luis Buenosvinos Zamora <>
2020-09-09 11:41:12 +0200

    no message

--- ticket-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/ticketing/application/command/NewOrderCommandHandler.java
+++ ticket-processor-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/ticketing/application/command/NewOrderCommandHandler.java
@@ -1,6 +1,7 @@
 package com.redhat.jbuenosv.ocpmicroservicesddd.sales.ticketing.application.command;
 
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.ticketing.domain.model.Order;
+import com.redhat.jbuenosv.ocpmicroservicesddd.sales.ticketing.infrastructure.store.kafka.order.OrderKafkaPublisher;
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.ticketing.infrastructure.store.kafka.order.OrderKafkaPublisherConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,14 +25,29 @@ public class NewOrderCommandHandler implements  CommandHandler {
      */
     @Override
     public void execute(Command command) {
+        OrderKafkaPublisher pub = null;
         NewOrderSubmittedCommand newOrderSubmittedCommand = (NewOrderSubmittedCommand)command;
         Order newOrder = newOrderSubmittedCommand.getOrder();
         String orderId = newOrder.getOrderId();
         String orderJson = newOrder.toJson();
         // String ordersTopicName = orderKafkaPublisherConfig.getKafkaOrdersTopicName() ;
         String ordersTopicName = "orders-commands";
+
         logger.debug("execute: [{},{},{}]",orderId,orderJson,ordersTopicName);
-        orderKafkaPublisherConfig.orderPublisher().publish(ordersTopicName,orderId,orderJson);
+
+        if (orderKafkaPublisherConfig != null) {
+            pub = orderKafkaPublisherConfig.orderPublisher();
+            if (pub != null) {
+                pub.publish(ordersTopicName,orderId,orderJson);
+            }
+            else {
+                logger.error("OrderKafkaPublisher is null :-(");
+            }
+        }
+        else {
+            logger.error("orderKafkaPublisherConfig is null :-(");
+        }
+
     }
 
 }
