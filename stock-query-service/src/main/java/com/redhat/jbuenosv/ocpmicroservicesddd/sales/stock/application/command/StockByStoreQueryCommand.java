8775d64fde6018ebc3c7cc5d7d25d5fa0e21901b
Juan Luis Buenosvinos Zamora <>
2020-09-12 22:59:02 +0200

    no message

--- stock-query-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/command/StockByStoreQueryCommand.java
+++ stock-query-service/src/main/java/com/redhat/jbuenosv/ocpmicroservicesddd/sales/stock/application/command/StockByStoreQueryCommand.java
@@ -1,10 +1,8 @@
 package com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.application.command;
 
 import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.StockValue;
-import com.redhat.jbuenosv.ocpmicroservicesddd.sales.stock.domain.model.StoreValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
